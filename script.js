72824b39ac028a7f4a246569df6b28041f990138
harsh <hdwivedi791@gmail>
2024-12-03 12:04:28 +0530

    for loop

---
+++ script.js
@@ -0,0 +1,14 @@
+function oddnumber(){
+    for(i=0;i<=10;i++){
+        if(i/2==0){
+        console.log(i+2);
+}
+else
+console.log(i)
+    }
+}
+function evennumber(){
+    for( i=0;i<=10;i=i+2){
+        console.log(i);
+    }
+}
